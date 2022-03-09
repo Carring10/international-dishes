9b15d462714dc184b48fbaff7c492b5c127977e3
Carrington <hartis.c99@gmail.com>
2022-03-09 10:02:50 -0500

    comments

---
+++ script.js
@@ -0,0 +1,35 @@
+var resultsContainer = document.getElementById("search-results-container");
+
+function getMealApi() {
+  // API URL for specified country.
+  var countryUrl =
+    "https://www.themealdb.com/api/json/v1/1/filter.php?a=Mexican";
+
+  fetch(countryUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+
+      for (let i = 0; i < 5; i++) {
+        var mealName = document.createElement("h2");
+        // Show ingredients button.
+        var btn = document.createElement("button");
+        mealName.textContent = data.meals[i].strMeal;
+        btn.textContent = "ingredients";
+        btn.setAttribute("id", data.meals[i].idMeal);
+        console.log(data.meals[i].strMeal);
+        resultsContainer.append(mealName);
+        resultsContainer.append(btn);
+
+        btn.addEventListener("click", getIngredients);
+      }
+    });
+}
+getMealApi();
+
+function getIngredients(e) {
+  var ingredientsUrl = "https://www.themealdb.com/api/json/v1/1/lookup.php?i=";
+  console.log(e.target.id);
+}
