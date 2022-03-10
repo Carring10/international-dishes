445ae3d36f5e35b93c35a3dc2f6100aaef5c35ed
Emily Kersey <ekerseyc@gmail.com>
2022-03-10 10:39:38 -0500

    Started on index

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+var recipeURL = "www.themealdb.com/api/json/v1/1/lookup.php?i=";
 var resultsContainer = document.getElementById("search-results-container");
 var cardContainer = document.getElementById("card-container");
 
@@ -17,20 +18,24 @@ function getMealApi() {
         // card HTML for recipes
         var card = document.createElement("div");
         card.setAttribute("class", "card");
+        card.addEventListener("click", getIngredients);
         var mealThumb = document.createElement("img");
         mealThumb.src = data.meals[i].strMealThumb;
         mealThumb.width = 250;
         var mealName = document.createElement("h3");
         mealName.textContent = data.meals[i].strMeal;
         mealThumb.textContent = data.meals[i].strMealThumb;
+        var recipeLink = document.createElement("a");
+        recipeLink.href = recipeURL + data.meals[i].idMeal;
+        recipeLink.textContent = "Click Here for Recipe";
         cardContainer.appendChild(card);
         card.append(mealName);
         card.append(mealThumb);
+        card.append(recipeLink);
         card.setAttribute("id", data.meals[i].idMeal);
         console.log(data.meals[i].strMeal);
-        // card.append(btn);
 
-        card.addEventListener("click", getIngredients);
+        
       }
     });
 }
@@ -40,10 +45,3 @@ function getIngredients(e) {
   var ingredientsUrl = "https://www.themealdb.com/api/json/v1/1/lookup.php?i=";
   console.log(e.target.id);
 }
-
-// LEAFLET MAP
-var map = L.map("map").setView([51.505, -0.09], 13);
-var attribution = __; // For copywrite.
-var titleUrl = __;
-var tileLayer = __;
-var apiUrl = __;
